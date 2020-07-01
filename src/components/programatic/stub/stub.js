4cecac01339625b3646647203deeb9edaabca862
kaihaan <kaihaan@gmail.com>
2020-07-01 18:48:18 +0100

    changed flex flow of anchors gfor blog page links

--- src/components/programatic/stub/stub.js
+++ src/components/programatic/stub/stub.js
@@ -8,14 +8,14 @@ export default ({ background, halign, post, link }) => {
     <Section background={background} key={post.id}>
       <Link to={link}>
         <ContainerMd>
+          <Image imageUrl={post.image.file.url} />
+        </ContainerMd>
+        <ContainerMd>
           <Item halign={halign}>
             <h3> {post.title} </h3>
             <h5>{post.author}</h5>
             <h5>{post.datePublished}</h5>
           </Item>
-        </ContainerMd>
-        <ContainerMd>
-          <Image imageUrl={post.image.file.url} />
         </ContainerMd>
       </Link>
     </Section>
